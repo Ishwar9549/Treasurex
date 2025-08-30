7060505bab0d9636f7f2a20f944a2adcac3b7284
ISHWAR MUNNOLLI <ishwarmunnolli420@gmail.com>
2025-08-30 13:52:13 +0530

    Add files via upload

---
+++ Login Copy2/src/main/java/com/example/Login_Page/DepartmentService1Application.java
@@ -0,0 +1,14 @@
+package com.example.Login_Page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentService1Application.class, args);
+	}
+
+}
+ 
