4090c164b2356b03410090668748a70e91624597
ISHWAR MUNNOLLI <ishwarmunnolli420@gmail.com>
2025-09-23 08:59:55 +0530

    Delete Login-service-9 directory

--- Login-service-9/src/main/java/com/treasurex/login_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.treasurex.login_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
